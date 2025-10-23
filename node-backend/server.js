1b5a95eabf30e6444f5274a0cde87fc45ea2a8d7
TavSingh <tavparsadsingh2@gmail.com>
2025-10-23 10:24:35 -0700

    Added a route folder and file.

--- node-backend/server.js
+++ node-backend/server.js
@@ -7,6 +7,9 @@ const PORT = 3000;
 app.use(cors());
 app.use(express.json());
 
+const route = require("./routes/route");
+app.use("/", route);
+
 app.listen(PORT, () => {
    console.log(`Server is running on http://localhost:${PORT}`);
 });
